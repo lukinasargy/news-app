23a93fa812cac1a7c85ef1b2728f7743fbf79e22
Sargy <lukina_sargy@mail.ru>
2020-01-21 17:01:19 +0300

    first commit deploy

---
+++ build/precache-manifest.1c5e87970c23dad10ca634a36eb0612c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710d8a0acce64b7e847293b483097545",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8eab3ed4e55a783af41e",
+    "url": "./static/css/main.7c2bfe30.chunk.css"
+  },
+  {
+    "revision": "be4619da57dc04d5f9e9",
+    "url": "./static/js/2.6c41d7e0.chunk.js"
+  },
+  {
+    "revision": "8eab3ed4e55a783af41e",
+    "url": "./static/js/main.e981fef1.chunk.js"
+  },
+  {
+    "revision": "9de16cf78ec41ed67a04",
+    "url": "./static/js/runtime-main.73603a9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
